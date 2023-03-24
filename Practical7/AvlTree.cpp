389659f465823117cfb7003a13b3690b399de037
Surykant Kasare <kasare9096464112@gmail.com>
2023-03-24 10:19:33 +0530

    Practical7

---
+++ Practical7/AvlTree.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+};
+Node* newNode(int data)
+{
+    Node* node=new Node();
+    node->data=data;
+    node->left=NULL;
+    node->right=NULL;
+   return node;
+}
+int getBala
+int main()
+{
+    
+}
