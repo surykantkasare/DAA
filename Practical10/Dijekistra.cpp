f65a69dad61ba1f549db16720da3ef63340c5f2b
Surykant Kasare <kasare9096464112@gmail.com>
2023-04-27 15:49:59 +0530

    Practical10

---
+++ Practical10/Dijekistra.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+void solve(){
+
+
+}
+int main(){
+solve();
+}
--- Practical8/knspsack.cpp
+++ Practical8/knspsack.cpp
@@ -1,13 +1,55 @@
+//{ Driver Code Starts
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
-ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
-ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
-void solve(){
+
+// } Driver Code Ends
+
+class Solution
+{
+	public:
+	//Function to find number of strongly connected components in the graph.
+    int kosaraju(int V, vector<vector<int>>& adj)
+    
+    {
+          vector<vector<int>>radj(adj.size());
+          for(auto it:adj)
+          {
+              for(auto i:it)
+              {
+                  radj[i].push_back(it);
+              }
+          }
+    }
+};
+
+//{ Driver Code Starts.
 
 
+int main()
+{
+    
+    int t;
+    cin >> t;
+    while(t--)
+    {
+    	int V, E;
+    	cin >> V >> E;
+
+    	vector<vector<int>> adj(V);
+
+    	for(int i = 0; i < E; i++)
+    	{
+    		int u, v;
+    		cin >> u >> v;
+    		adj[u].push_back(v);
+    	}
+
+    	Solution obj;
+    	cout << obj.kosaraju(V, adj) << "\n";
+    }
+
+    return 0;
 }
-int main(){
-solve();
-}
+
+
+// } Driver Code Ends
